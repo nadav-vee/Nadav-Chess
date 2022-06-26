1ba4cc438f7e53b4c6068d319a492b0b2503e7f1
Nadav Aviran <nadav.aviran@kryonsystems.com>
2022-06-27 01:43:16 +0300

    server + client

--- server.py
+++ server.py
@@ -24,9 +24,12 @@ class server:
         self.MAX_MSG_LENGTH = cp.MAX_MSG_LENGTH
         self.server_name = cp.server_name
         self.device_list = []
+        self.device_match = []
         self.ready_to_read = []
         self.ready_to_write = []
         self.in_error = []
+        self.devone_wait = False
+        self.devtwo_wait = False
         self.server_device = remoteDevice(self.s, self.IP)
         self.requests_queue = queue.SimpleQueue()
 
@@ -60,10 +63,14 @@ class server:
     def add_conn(self, conn, add):
         new_device = remoteDevice(conn, add)
         self.device_list.append(new_device)
+        if len(self.device_match) < 2 and new_device not in self.device_match:
+            self.device_match.append(new_device)
+
 
     def disconnect_device(self, device):
         self.logger.info(f"connection with client {device.fileno()} is closed")
         self.device_list.remove(device)
+        device.conn.close()
 
     def print_client_sockets(self):
         for c in self.device_list:
@@ -114,12 +121,6 @@ class server:
             print(msg)
             self.logger.info(msg)
 
-    def match_making(self):
-        while self.requests_queue.qsize() > 1:
-            dev1 = self.requests_queue.get()
-            dev2 = self.requests_queue.get()
-            self.connect_two_devices(dev1, dev2)
-
     def start(self):
         self.s.bind((self.IP, self.PORT))
         run = True
@@ -128,6 +129,8 @@ class server:
         self.logger.info("Listening for clients...")
         print("Server is up and running...")
         print("Listening for clients...")
+        dev1 = None
+        dev2 = None
         while run:
             print(self.requests_queue.qsize())
             self.ready_to_read, self.ready_to_write, self.in_error = select.select([self.server_device] + self.device_list, [], [])
@@ -143,11 +146,35 @@ class server:
                         _type, _msg = self.recv_and_parse(current_device.conn)
                         print( "New data from client " + _type)
                         if _type == "REQUEST_OPPONENT":
-                            self.requests_queue.put(current_device)
-                            self.send_wait(current_device.conn)
+                            if len(self.device_match) == 1:
+                                self.send_wait(current_device.conn)
+                                self.devone_wait = True
+                            elif len(self.device_match) == 2:
+                                self.send_wait(current_device.conn)
+                                self.devtwo_wait = True
+                            else:
+                                self.requests_queue.put(current_device)
+                        if _type == "LISTENING":
+                            self.build_and_send(self.device_match[1].conn, "ATTEMPT_CONN", "")
+                        if _type == "CONNECTING":
+                            self.build_and_send(self.device_match[0].conn, "ACCEPT_CLIENT", "")
+                        if _type == "CONT":
+                            msg = (f"successfully orchestrated match between: \n"
+                                   f"{socket.gethostbyaddr(self.device_match[0].address[0])} "
+                                   f"and {socket.gethostbyaddr(self.device_match[1].address[0])}")
+                            print(msg)
+                            self.logger.info(msg)
                         if _type == cp.disconnect_msg:
                             self.disconnect_device(device=current_device)
-            self.match_making()
+            if self.devone_wait and self.devtwo_wait:
+                self.devone_wait = False
+                self.devtwo_wait = False
+                self.send_ok(self.device_match[0].conn)
+                self.send_ok(self.device_match[1].conn)
+                self.build_and_send(self.device_match[0].conn, "IS_LISTEN", "1")
+                print("sent is listen")
+                self.build_and_send(self.device_match[1].conn, "IP_ADDRESS", self.device_match[0].address[0])
+                print(f"sent ip address {self.device_match[0].conn}, {self.device_match[0].address[0]} IP_ADDRESS")
 
     def __del__(self):
         self.s.close()
